b06a64077eabfed56caa5f6cfb6cb206faa0a67a
ellinor <ellinoora.riskala@metropolia.fi>
2024-08-28 11:50:18 +0300

    update

--- dom-assignments/t5/t5.js
+++ dom-assignments/t5/t5.js
@@ -771,3 +771,29 @@ const restaurants = [
 ];
 
 // your code here
+'use strict';
+
+const map = L.map('map').setView([60.2, 25], 12);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 20
+}).addTo(map);
+
+restaurants.forEach(restaurant => {
+  const [longitude, latitude] = restaurant.location.coordinates;
+  const marker = L.marker([latitude, longitude]).addTo(map);
+
+  const popUp = document.createElement('div');
+
+  const name = document.createElement('h3');
+  name.textContent = `Restaurant: ${restaurant.name}`;
+
+  const address = document.createElement('p');
+  address.textContent = `Address: ${restaurant.address}`;
+
+  popUp.appendChild(name);
+  popUp.appendChild(address);
+
+  marker.bindPopup(popUp);
+});
+
